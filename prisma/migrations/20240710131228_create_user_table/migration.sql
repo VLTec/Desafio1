cf4206fbd25c4514f1221f8ce5debbc689b50a5a
Ricardo Machado <ricardo.machado10.rm@gmail.com>
2024-07-10 10:16:54 -0300

    fix: conf to run the project

--- prisma/migrations/20240710131228_create_user_table/migration.sql
+++ prisma/migrations/20240710131228_create_user_table/migration.sql
@@ -1,13 +1,11 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateIndex
