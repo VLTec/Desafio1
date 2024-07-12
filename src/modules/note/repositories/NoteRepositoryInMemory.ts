6a769658ffd6e4a803a237499ef928953e165b41
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-11 22:59:42 -0300

    feat:creation of the repositories folder which is responsible for creating the CRUD methods where we will have an in-memory database to facilitate testing

---
+++ src/modules/note/repositories/NoteRepositoryInMemory.ts
@@ -0,0 +1,36 @@
+import { Note } from "../entities/Note";
+import { NoteRepository } from "./NoteRepository";
+
+export class NoteRepositoryInMemory implements NoteRepository {
+    public notes: Note[] = [];
+
+    async create(note: Note): Promise<void> {
+        this.notes.push(note);
+    }
+
+    async update(note: Note): Promise<void> {
+        this.notes = this.notes.map((n) => (n.id === note.id ? note : n))
+    }
+
+    async findById(id: string): Promise<Note | null> {
+        const note = this.notes.find((n) => n.id === id);
+
+        if(!note) {
+            return null;
+        }
+
+        return note;
+    }
+
+    async findAll(): Promise<Note[] | null> {
+        if (this.notes.length === 0) {
+            return null;
+        }
+
+        return this.notes;
+    }
+
+    async delete(id: string): Promise<void> {
+        this.notes = this.notes.filter((n) => n.id !== id);
+    }
+}
