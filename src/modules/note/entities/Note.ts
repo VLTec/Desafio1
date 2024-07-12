111786134c8903f3569edfe1dff359edff4d48cb
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-11 22:54:20 -0300

    feat:creation of the entities folder that is responsible for the structure of notes

---
+++ src/modules/note/entities/Note.ts
@@ -0,0 +1,63 @@
+import { randomUUID } from 'crypto';
+import { Replace } from 'src/types/replace';
+
+interface NoteSchema {
+    title: string;
+    userId: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export class Note {
+    private props: NoteSchema;
+    private _id: string;
+
+    constructor(
+        props: Replace<NoteSchema, { createdAt?: Date; updatedAt?: Date }>,
+        id?: string
+    ) {
+        this.props = {
+            ...props,
+            createdAt: props.createdAt || new Date(),
+            updatedAt: props.updatedAt || new Date(),
+        };
+        this._id = id || randomUUID();
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    get userId(): string {
+        return this.props.userId;
+    }
+
+    get updateAt(): Date {
+        return this.props.updatedAt;
+    }
+
+    set updateAt(updatedAt: Date) {
+        this.props.updatedAt = updatedAt;
+    }
+
+    get title(): string {
+        return this.props.title;
+    }
+
+    set title(title: string) {
+        this.props.title = title;
+    }
+
+    get description(): string {
+        return this.props.description;
+    }
+
+    set description(description: string) {
+        this.props.description = description;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+}
