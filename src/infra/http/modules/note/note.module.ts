1e8fcc662fe3b67b93d7a360e0b7fba66613cb50
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-12 21:33:37 -0300

    feat:creation of the note module responsible for grouping all note logic

---
+++ src/infra/http/modules/note/note.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { NoteController } from "./note.controller";
+import { CreateNoteUseCase } from "src/modules/note/useCases/createNoteUseCase/createNoteUseCase";
+import { UpdateNoteUseCase } from "src/modules/note/useCases/updateNoteUseCase/updateNoteUseCase";
+import { GetNoteUseCase } from "src/modules/note/useCases/getNoteUseCase/getNoteUseCase";
+import { GetAllNotesUseCase } from "src/modules/note/useCases/getAllNotesUseCase/getAllNotesUseCase";
+import { DeleteNoteUseCase } from "src/modules/note/useCases/deleteNoteUseCase/deleteNoteUseCase";
+import { DatabaseModule } from "src/infra/database/database.module";
+
+@Module({
+    imports: [DatabaseModule],
+    controllers: [NoteController],
+    providers: [
+        CreateNoteUseCase,
+        UpdateNoteUseCase,
+        GetNoteUseCase,
+        GetAllNotesUseCase,
+        DeleteNoteUseCase
+    ]
+})
+
+export class NoteModule {}
