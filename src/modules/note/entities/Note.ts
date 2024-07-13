fce76d90c8fdf79c20f4491fa3b77dbab39b1e11
yLexter <lucasmaia5954@gmail.com>
2024-07-13 10:31:22 -0300

    feat: add note entity

---
+++ src/modules/note/entities/Note.ts
@@ -0,0 +1,50 @@
+import { randomUUID } from 'crypto';
+import { Replace } from 'src/types/replace';
+
+export interface NoteSchema {
+  title: string;
+  userId: string;
+  description: string;
+  createdAt: Date;
+}
+
+export class Note {
+  private props: NoteSchema;
+  private _id: string;
+
+  constructor(props: Replace<NoteSchema, { createdAt?: Date; }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt || new Date(),
+    };
+    this._id = randomUUID() ;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get userId(): string {
+    return this.props.userId;
+  }
+
+  get title(): string {
+    return this.props.title;
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+  }
+
+  get description(): string | undefined {
+    return this.props.description;
+  }
+
+  set description(description: string) {
+    this.props.description = description;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
