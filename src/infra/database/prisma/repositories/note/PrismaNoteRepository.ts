ee6a508752fc21d2a5c75f2a24e6606558d774cd
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-12 21:39:10 -0300

    feat:creation of database repositories responsible for storing application logic using Prisma

---
+++ src/infra/database/prisma/repositories/note/PrismaNoteRepository.ts
@@ -0,0 +1,58 @@
+import { Note } from "src/modules/note/entities/Note";
+import { NoteRepository } from "src/modules/note/repositories/NoteRepository";
+import { Injectable } from "@nestjs/common";
+import { PrismaNoteMapper } from "../../mappers/note/PrismaNoteMapper";
+import { PrismaService } from "../../prisma.service";
+
+@Injectable()
+export class PrismaNoteRepository implements NoteRepository {
+    constructor(private prisma: PrismaService) {}
+
+    async create(note: Note): Promise<void> {
+        const noteRaw = PrismaNoteMapper.toPrisma(note);
+
+        await this.prisma.note.create({
+            data: noteRaw
+        })
+    }
+
+    async update(note: Note): Promise<void> {
+        const data = PrismaNoteMapper.toPrisma(note);
+
+        await this.prisma.note.update({
+            where: {
+                id: note.id
+            },
+
+            data
+        });
+    }
+
+    async findById(id: string): Promise<Note | null> {
+        const note = await this.prisma.note.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!note) return null
+        
+        return PrismaNoteMapper.toDomain(note);
+    }
+
+    async findAll(): Promise<Note[] | null> {
+        const notes = await this.prisma.note.findMany();
+
+        if (!notes) return null
+
+        return (notes.map((note) => PrismaNoteMapper.toDomain(note)));
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.prisma.note.delete({
+            where: {
+                id
+            }
+        });
+    }
+}
