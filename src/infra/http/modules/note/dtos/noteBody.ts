b22a51fde77ffa6930c72ec226bcb593a2278032
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-07-13 15:57:42 -0300

    fix: ajust note body request

--- src/infra/http/modules/note/dtos/noteBody.ts
+++ src/infra/http/modules/note/dtos/noteBody.ts
@@ -1,12 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmptyCustom } from 'src/infra/http/classValidator/decorators/IsNotEmptyCustom';
 
 export class NoteBody {
+  @IsNotEmptyCustom()
   @ApiProperty()
   title: string;
 
-  @ApiProperty()
-  description?: string;
+  @ApiProperty({ required: false })
+  description: string;
 
+  @IsNotEmptyCustom()
   @ApiProperty()
   note: string;
 }
