5192c240c725b386deb01b70a1148a6621dfa80c
josué <josuesilva.js34.js@gmail.com>
2024-07-12 22:50:10 -0300

    chore: setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { UserModule } from './infra/http/modules/user/user.module';
 import { DatabaseModule } from './infra/database/database.module';
 import { AuthModule } from './infra/http/modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './infra/http/modules/auth/guards/jwtAuth.guard';
-
-import { MailerModule } from '@nestjs-modules/mailer';
 import { env } from './env';
 @Module({
   imports: [DatabaseModule, UserModule, AuthModule],
