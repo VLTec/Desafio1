8d5743a85b846c2650c913b121280444e9a205ae
Gustavo Silva <gugassilva2000@gmail.com>
2024-07-15 23:48:02 -0300

    :zap: fix note structure

--- src/note/dto/update-note.dto.ts
+++ src/note/dto/update-note.dto.ts
@@ -1,9 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateNoteDto } from './create-note.dto';
-import { IsString } from 'class-validator';
 
-export class UpdateNoteDto extends PartialType(CreateNoteDto) {
-  @ApiProperty()
-  @IsString()
-  id: string;
-}
+export class UpdateNoteDto extends PartialType(CreateNoteDto) {}
